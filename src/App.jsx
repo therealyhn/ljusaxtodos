5b0a4af072ea6b300388264ab100c7a73ba6aa8d
Jovan Ljusic <jovanljusicum@outlook.com>
2025-09-08 11:36:22 +0200

    Init-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <h1 className="text-4xl font-bold text-center mt-10 animate__animated animate__fadeIn">
+      Welcome to LJUSA x TODOS Portfolio 🎶
+    </h1>
+  )
+}
+
+export default App
