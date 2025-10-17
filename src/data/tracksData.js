1e51f696849039f33b63bcfa70e77c215ddcae44
Jovan Ljusic <jovanljusicum@outlook.com>
2025-10-17 10:56:41 +0200

    test

--- src/data/tracksData.js
+++ src/data/tracksData.js
@@ -3,37 +3,37 @@ export default [
     title: "Rasta - Tajland",
     artist: "LJUSA Mashup",
     src: "/audio/Tajland-Yhn.mp3",
-    duration: "5:28",
+    duration: "3:00",
   },
   {
     title: "Space Disko Heineken",
     artist: "TODOS Mashup",
     src: "/audio/Spaceman-Todos.wav",
-    duration: "6:18",
+    duration: "5:05",
   },
   {
     title: "Hi Hi Gasoliona",
     artist: "LJUSA Transition Mashup",
     src: "/audio/Hi-Hi-Gasolina-Yhn.mp3",
-    duration: "5:28",
+    duration: "3:32",
   },
   {
     title: "Destination Fantazija",
     artist: "TODOS Mashup",
     src: "/audio/Fantazija-Todos.mp3",
-    duration: "6:18",
+    duration: "2:27",
   },
   {
     title: "Hardwell vs. Devito & Voyage - Acid Mafia",
     artist: "LJUSA Mashup",
     src: "/audio/Acid-Mafia-Yhn.mp3",
-    duration: "5:28",
+    duration: "2:48",
   },
   {
     title: "HILJSON MANDELA x MIACH - ANDJEO",
     artist: "TODOS Mashup",
     src: "/audio/industrial-pulse.mp3",
-    duration: "6:18",
+    duration: "3:00",
   },
   
 ];
