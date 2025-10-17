c1ef37076177ea38c25874dee7e0dac3236642bf
Jovan Ljusic <jovanljusicum@outlook.com>
2025-10-17 10:54:17 +0200

    test tracks

---
+++ src/data/tracksData.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    title: "Rasta - Tajland",
+    artist: "LJUSA Mashup",
+    src: "/audio/Tajland-Yhn.mp3",
+    duration: "5:28",
+  },
+  {
+    title: "Space Disko Heineken",
+    artist: "TODOS Mashup",
+    src: "/audio/Spaceman-Todos.wav",
+    duration: "6:18",
+  },
+  {
+    title: "Hi Hi Gasoliona",
+    artist: "LJUSA Transition Mashup",
+    src: "/audio/Hi-Hi-Gasolina-Yhn.mp3",
+    duration: "5:28",
+  },
+  {
+    title: "Destination Fantazija",
+    artist: "TODOS Mashup",
+    src: "/audio/Fantazija-Todos.mp3",
+    duration: "6:18",
+  },
+  {
+    title: "Stockholm Nights",
+    artist: "Ljusa",
+    src: "/audio/stockholm-nights.mp3",
+    duration: "5:28",
+  },
+  {
+    title: "Industrial Pulse",
+    artist: "Todos",
+    src: "/audio/industrial-pulse.mp3",
+    duration: "6:18",
+  },
+  
+];
